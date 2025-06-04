52882dc53b42d4f23b9f7fedadcee336282317ba
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-06-04 16:03:05 +0800

    Refactor project rendering and feedback display; replace filter.js with project-list.js and enhance user feedback on project actions

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import {
   updateProject,
 } from "./data/projects.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
-import { renderProjectsList } from "./scripts/filter.js";
+import { renderProjectsList } from "./scripts/project-list.js";
+import { capitalizeFirstLetter } from "./utils/format-text.js";
 
 renderProjects();
 const todayElement = document.querySelector(".js-date");
@@ -38,15 +39,11 @@ addProjectForm.addEventListener("submit", (event) => {
   const projectNotes = document.querySelector(".js-project-notes-input").value;
 
   addProject(projectName, projectStatus, projectDueDate, projectNotes);
-  document.querySelector(".js-added-message").classList.remove("hide");
-
-  setTimeout(() => {
-    document.querySelector(".js-added-message").classList.add("hide");
-  }, 2000);
-
   renderProjects();
   addProjectForm.reset();
   addProjectForm.classList.add("hide");
+
+  displayFeedbackMessage(`Project "${projectName}" added successfully!`);
 });
 
 function renderProjects() {
@@ -71,7 +68,142 @@ function renderProjects() {
       const projectId = button.dataset.projectId;
       removeProject(projectId);
       renderProjects();
+
+      displayFeedbackMessage(`Project removed successfully!`);
     });
+  });
+
+  document
+    .querySelector(".js-planned-filter-btn")
+    .addEventListener("click", () => {
+      const plannedFilter = [];
+      projects.filter((project) => {
+        if (project.status === "planned") {
+          plannedFilter.push(project);
+        }
+      });
+      document.querySelector(
+        ".js-project-quantity"
+      ).innerHTML = `(${plannedFilter.length})`;
+      document.querySelector(".js-project-list").innerHTML =
+        renderProjectsList(plannedFilter);
+      document.querySelectorAll(".js-edit-status-btn").forEach((button) => {
+        button.addEventListener("click", () => {
+          const projectId = button.dataset.projectId;
+          document
+            .querySelectorAll(`.js-edit-input-${projectId}`)
+            .forEach((inputElement) => {
+              inputElement.classList.toggle("hide");
+            });
+          document
+            .querySelectorAll(`.js-project-detail-${projectId}`)
+            .forEach((element) => {
+              element.classList.toggle("hide");
+            });
+
+          document
+            .querySelectorAll(`.js-save-btn-${projectId}`)
+            .forEach((saveButton) => {
+              saveButton.classList.toggle("hide");
+            });
+
+          document.querySelectorAll(".js-save-btn").forEach((button) => {
+            button.addEventListener("click", () => {
+              const projectNameInput = document.querySelector(
+                `.js-edit-project-name-${projectId}`
+              );
+              const projectStatusInput = document.querySelector(
+                `.js-edit-project-status-${projectId}`
+              );
+              const projectDueDateInput = document.querySelector(
+                `.js-edit-project-duedate-${projectId}`
+              );
+              const projectNotesInput = document.querySelector(
+                `.js-edit-project-notes-${projectId}`
+              );
+              updateProject(
+                projectId,
+                projectNameInput.value,
+                projectStatusInput.value,
+                projectDueDateInput.value,
+                projectNotesInput.value
+              );
+              renderProjects();
+              console.log(projects);
+            });
+          });
+        });
+      });
+    });
+
+  document
+    .querySelector(".js-ongoing-filter-btn")
+    .addEventListener("click", () => {
+      const ongoingFilter = [];
+      projects.filter((project) => {
+        if (project.status === "ongoing") {
+          ongoingFilter.push(project);
+        }
+      });
+      document.querySelector(
+        ".js-project-quantity"
+      ).innerHTML = `(${ongoingFilter.length})`;
+      document.querySelector(".js-project-list").innerHTML =
+        renderProjectsList(ongoingFilter);
+      document.querySelectorAll(".js-edit-status-btn").forEach((button) => {
+        button.addEventListener("click", () => {
+          const projectId = button.dataset.projectId;
+
+          document
+            .querySelectorAll(`.js-edit-input-${projectId}`)
+            .forEach((inputElement) => {
+              inputElement.classList.toggle("hide");
+            });
+          document
+            .querySelectorAll(`.js-project-detail-${projectId}`)
+            .forEach((element) => {
+              element.classList.toggle("hide");
+            });
+
+          document
+            .querySelectorAll(`.js-save-btn-${projectId}`)
+            .forEach((saveButton) => {
+              saveButton.classList.toggle("hide");
+            });
+
+          document.querySelectorAll(".js-save-btn").forEach((button) => {
+            button.addEventListener("click", () => {
+              const projectNameInput = document.querySelector(
+                `.js-edit-project-name-${projectId}`
+              );
+              const projectStatusInput = document.querySelector(
+                `.js-edit-project-status-${projectId}`
+              );
+              const projectDueDateInput = document.querySelector(
+                `.js-edit-project-duedate-${projectId}`
+              );
+              const projectNotesInput = document.querySelector(
+                `.js-edit-project-notes-${projectId}`
+              );
+              updateProject(
+                projectId,
+                projectNameInput.value,
+                projectStatusInput.value,
+                projectDueDateInput.value,
+                projectNotesInput.value
+              );
+              renderProjects();
+              console.log(projects);
+
+              displayFeedbackMessage(`Project updated successfully!`);
+            });
+          });
+        });
+      });
+    });
+
+  document.querySelector(".js-all-filter-btn").addEventListener("click", () => {
+    renderProjects();
   });
 
   document.querySelectorAll(".js-edit-status-btn").forEach((button) => {
@@ -117,46 +249,20 @@ function renderProjects() {
           );
           renderProjects();
           console.log(projects);
+
+          displayFeedbackMessage(`Project updated successfully!`);
         });
       });
     });
   });
-
-  document
-    .querySelector(".js-planned-filter-btn")
-    .addEventListener("click", () => {
-      const plannedFilter = [];
-      projects.filter((project) => {
-        if (project.status === "planned") {
-          plannedFilter.push(project);
-        }
-      });
-      document.querySelector(
-        ".js-project-quantity"
-      ).innerHTML = `(${plannedFilter.length})`;
-      document.querySelector(".js-project-list").innerHTML =
-        renderProjectsList(plannedFilter);
-      console.log(plannedFilter);
-    });
-
-  document
-    .querySelector(".js-ongoing-filter-btn")
-    .addEventListener("click", () => {
-      const ongoingFilter = [];
-      projects.filter((project) => {
-        if (project.status === "ongoing") {
-          ongoingFilter.push(project);
-        }
-      });
-      document.querySelector(
-        ".js-project-quantity"
-      ).innerHTML = `(${ongoingFilter.length})`;
-      document.querySelector(".js-project-list").innerHTML =
-        renderProjectsList(ongoingFilter);
-      console.log(ongoingFilter);
-    });
-
-  document.querySelector(".js-all-filter-btn").addEventListener("click", () => {
-    renderProjects();
-  });
 }
+
+function displayFeedbackMessage(message) {
+  const bottomMessageElement = document.querySelector(".js-added-message");
+  bottomMessageElement.classList.remove("hide");
+  bottomMessageElement.innerHTML = `${message}`;
+  setTimeout(() => {
+    bottomMessageElement.classList.add("hide");
+  }, 2000);
+}
+
